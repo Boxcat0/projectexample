a57b8eb4929baaaa602f0d564f639e44510f3a9b
Boxcat0 <vv5454@naver.com>
2022-10-05 19:13:30 +0900

    commit

--- src/main/kotlin/com/example/project/main/Review/ReviewController.kt
+++ src/main/kotlin/com/example/project/main/Review/ReviewController.kt
@@ -2,23 +2,25 @@ package com.example.project.main.Review
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
-import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
 class ReviewController(val service: ReviewService) {
-    @RequestMapping("/allreview")
-    fun review(model : Model): String{
+    @GetMapping("/allreview")
+    fun re(): List<Review> = service.findReviews()
+    /*fun review(model : Model): String{
         val re: List<Review> = service.findReviews()
         model.addAttribute("re", re)
-        return "reviewer.html"
-    }
+        return "reviewer"
+    }*/
+
     @PostMapping("/allreview")
-    fun  reviewpost(@RequestBody review: Review) {
+    fun  reviewpost(@RequestBody review: Review)
+    {
         service.reviewpost(review)
     }
 }
